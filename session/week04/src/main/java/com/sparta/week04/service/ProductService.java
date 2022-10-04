b716da46b94efc466eccf852c4c512fdb1e50248
OaYenn <amy350500@gmail.com>
2022-10-04 16:48:35 +0900

    스프링 4주차 스터디 완료

---
+++ session/week04/src/main/java/com/sparta/week04/service/ProductService.java
@@ -0,0 +1,35 @@
+package com.sparta.week04.service;
+
+import com.sparta.week04.domain.ItemDto;
+import com.sparta.week04.domain.Product;
+import com.sparta.week04.domain.ProductMypriceRequestDto;
+import com.sparta.week04.domain.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor // final로 선언된 멤버변수를 자동으로 생성
+@Service // 서비스임을 선언
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Transactional // 메소드 동작이 SQL 쿼리문임을 선언
+    public Long update(Long id, ProductMypriceRequestDto requestDto) {
+        Product product = productRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("해당 아이디가 존재하지 않습니다.")
+        );
+        product.update(requestDto);
+        return id;
+    }
+
+    @Transactional // 메소드 동작이 SQL 쿼리문임을 선언
+    public Long updateBySearch(Long id, ItemDto itemDto) {
+        Product product = productRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("해당 아이디가 존재하지 않습니다.")
+        );
+        product.updateByItemDto(itemDto);
+        return id;
+    }
+}
